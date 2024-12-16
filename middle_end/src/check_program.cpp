10e647ff2a3f77471c9dcef08edc494c36c45753
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-12-16 22:05:56 +0300

    Added checking parametrs amount

--- middle_end/src/check_program.cpp
+++ middle_end/src/check_program.cpp
@@ -1,4 +1,5 @@
 #include "check_program.h"
+#include <assert.h>
 
 err_code_t check_program(my_tree_t* prog, nametable_t nametable)
 {
@@ -61,13 +62,51 @@ err_code_t check_definitions(my_tree_t* prog, nametable_t nametable)
     return OK;
 }
 
-err_code_t find_func_def()
+size_t count_parametrs(node_t* node)
 {
+    assert(node->type == STATEMENT && ((int) node->data == SEPARATOR));
+    static size_t counter = 0;
+    if (node->parent->type == STATEMENT && (int) node->parent->data == FUNC_SPEC)
+    {
+        counter = 0;
+    }
+    if (node->left  != NULL) counter++;
+    if (node->right != NULL) count_parametrs(node->right);
+    return counter;
+}
+
+err_code_t check_all_calls(my_tree_t* prog, node_t* node, size_t params_amount)
+{
+    if (node->type == STATEMENT && (int) node->data == FUNC_SPEC)
+    {
+        size_t func_params = count_parametrs(node->right);
+        if (func_params != params_amount)
+        {
+            fprintf(stderr, "In func %s should be %zu params, but %zu instead\n", *(char**)&node->left->data, params_amount, func_params);
+            abort();
+        }
+    }
+
+    if (node->left  != NULL) check_all_calls(prog, node->left,  params_amount);
+    if (node->right != NULL) check_all_calls(prog, node->right, params_amount);
+
     return OK;
 }
 
 err_code_t check_params_num(my_tree_t* prog, nametable_t nametable)
 {
+    for (size_t i = 0; i < MAX_ID_COUNT; i++)
+    {
+        identificator id = nametable[i];
+        if (id.name[0] == '\0') break;
+
+        if (id.type == FUNC_TYPE)
+        {
+            size_t def_params = count_parametrs(find_id_decl(prog, prog->root, id.name, false)->left->right);
+            check_all_calls(prog, prog->root, def_params);
+        }
+    }
+
     return OK;
 }
 
